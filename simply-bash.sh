825da2a607d7a38a40f52564f201c7c2dc096e62
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2018-01-23 21:42:05 +0000

    Export include and SIMPLY_BASH_PATH

--- simply-bash.sh
+++ simply-bash.sh
@@ -4,16 +4,20 @@
 
 readonly SIMPLY_BASH_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)
 
-# Make all scripts visible
 PATH="${PATH}:${SIMPLY_BASH_PATH}"
 
 # $1 - library file
+# NOTE: include needs to be a function that gets sourced. If it would be a bash
+#       script `source` would not work.
 include() {
     local file=$1
-
     source "${SIMPLY_BASH_PATH}/${file}"
 }
 
 # Make python environment functions available
 include 'pyenvs.sh'
 include 'completion/activate-completion.bash'
+
+# Make visible in other functions
+export SIMPLY_BASH_PATH
+export -f include
