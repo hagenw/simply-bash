be31c4aaacce2d700588469fb9fcec6a6b66e8e9
Hagen Wierstorf <hagenw@posteo.de>
2018-11-23 12:06:21 +0100

    Add envs list command

--- completion/envs-completion.bash
+++ completion/envs-completion.bash
@@ -4,7 +4,7 @@
 _envs() {
     local current arguments
     current="${COMP_WORDS[COMP_CWORD]}"
-    arguments="conda virtualenv help tool size location"
+    arguments="conda virtualenv help tool size location list"
     COMPREPLY=( $(compgen -W "${arguments}" -- ${current}) )
     return 0
 }
